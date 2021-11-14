ed18faca588974560c17be65f04be9a3ed2bebee
RRotoko <91010024+RRotoko@users.noreply.github.com>
2021-11-14 21:51:49 +0100

    VESTBERRY-Test-Assignment
    VESTBERRY-Test-Assignment

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './pages/App/App'
+
+// Render Setup
+// ------------------------------------
+const MOUNT_NODE = document.getElementById('root')
+
+const render = () => {
+
+  ReactDOM.render(
+    <App />,
+    MOUNT_NODE
+  )
+}
+
+// Let's Go!
+// ------------------------------------
+if (!__TEST__) render()
